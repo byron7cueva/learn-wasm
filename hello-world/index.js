11b979764db0cf0327d87f7735a34df96892bbc2
byron7cueva <byron7cueva@gmail.com>
2020-06-25 11:52:10 -0500

    Compartiendo memoria js y wasm

--- hello-world/index.js
+++ hello-world/index.js
@@ -3,17 +3,48 @@ import init from './pkg/hello_world.js'
 
 const runWasm = async () => {
   // Inicializando el modulo wasm
-  const helloWWord = await init('./pkg/hello_world_bg.wasm')
+  const helloWorld = await init('./pkg/hello_world_bg.wasm')
 
   // Llamando a la funcion add exportada desde wasm y guardando el resultado
-  const addResult = helloWWord.add(24, 24)
+  const addResult = helloWorld.add(24, 24)
 
-  const result = helloWWord.call_me_from_javascript(24, 24)
+  const result = helloWorld.call_me_from_javascript(24, 24)
 
   // Añadiendo el resultado dentro del body
   document.body.textContent = `Hola mundo addResult: ${addResult}`
 
   console.log(result)
+
+
+  // Primera parte escribiendo desde wasm y leendo desde javascript
+  // Escribiendo en el buffer de wasm
+  console.log('Write in wasm and read from js, index 0')
+  helloWorld.store_value_in_wasm_memory_buffer_index_zero(24)
+
+  // Creando un Unit8Array del wasm buffer
+  let wasmMemory = new Uint8Array(helloWorld.memory.buffer)
+
+  // Obtener el puntero del buffer que esta dentro de helloWorld
+  let bufferPointer = helloWorld.get_wasm_memory_buffer_pointer()
+
+  // Leendo el valor escrito en el indice 0 del buffer
+  console.log(wasmMemory[bufferPointer + 0])
+
+  // Segunda parte escribiendo en js y leendo desde wasm
+  console.log('Write in js, read in wasm, index 1')
+  // Escribiendo en el indice 1 en el buffer
+  wasmMemory[bufferPointer + 1] = 25
+
+  // Leendo desde wasm el buffer en la posicion 1 y retornando el resultado
+  console.log(helloWorld.read_wasm_memory_buffer_and_return_index_one())
+
+  /**
+  * NOTA: si tuviéramos que seguir leyendo y escribiendo memoria,
+  * Dependiendo de cómo crece la memoria en rust, es posible que tenga
+  * que volver a crear el Uint8Array ya que el diseño de la memoria podría cambiar.
+  * Por ejemplo, `let wasmMemory = new Uint8Array (rustWasm.memory.buffer);`
+  * En este ejemplo, no lo hicimos, pero tenga en cuenta que esto puede suceder
+   */
 }
 
 runWasm()
--- hello-world/src/lib.rs
+++ hello-world/src/lib.rs
@@ -1,6 +1,14 @@
+// wasm-pack usa wasm-bindgen para construir y generar Javascript binding file
 use wasm_bindgen::prelude::*;
 
 const ADD_CONSTANT: i32 = 24;
+
+const WASM_MEMORY_BUFFER_SIZE: usize = 2;
+
+// global y static mut pueden ser unsafe code
+// Pero para pasar memoria entre js y wasm podria estar bien
+// Se crea un array de 2 posicione sin signo de 8bytes y se lo llena de 0s
+static mut WASM_MEMORY_BUFFER: [u8; WASM_MEMORY_BUFFER_SIZE] = [0; WASM_MEMORY_BUFFER_SIZE];
 
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
@@ -15,4 +23,38 @@ pub fn call_me_from_javascript(a: i32, b: i32) -> i32 {
 
 fn add_integer_with_constant(a: i32, b: i32) -> i32 {
     a + b + ADD_CONSTANT
+}
+
+/*
+Funcion para guardar el valor pasado en el indice 0 en el buffer
+*/
+#[wasm_bindgen] 
+pub fn store_value_in_wasm_memory_buffer_index_zero(value: u8) {
+    unsafe {
+        WASM_MEMORY_BUFFER[0] = value;
+    }
+}
+
+/*
+Funcion que retorna el apuntador del buffer en memoria de wasm
+*/
+#[wasm_bindgen]
+pub fn get_wasm_memory_buffer_pointer() -> *const u8 {
+    let pointer: *const u8;
+    unsafe {
+        pointer = WASM_MEMORY_BUFFER.as_ptr();
+    }
+    pointer
+}
+
+/*
+Funcion que lee el indice 1 del buffer y retorna el valor del indice
+*/
+#[wasm_bindgen]
+pub fn read_wasm_memory_buffer_and_return_index_one() -> u8 {
+    let value: u8;
+    unsafe  {
+        value = WASM_MEMORY_BUFFER[1];
+    }
+    value
 }
